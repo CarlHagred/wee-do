8ca83559b4bbe0b184392e6c1e172d40f31d2d3f
CarlHagred <carl.hagred@gmail.com>
2021-10-13 15:56:11 +0200

    Addad a scanner
    You can now scan qr codes and the component will also handle errors

--- client/src/pages/Scanner.js
+++ client/src/pages/Scanner.js
@@ -1,14 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useHistory, Prompt } from 'react-router-dom';
+import QrReader from 'react-web-qr-reader';
 
 const ScanQr = () => {
+  const history = useHistory();
+
+  const [failedScan, setFailedScan] = useState(false);
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  const handleScan = (result) => {
+    setFailedScan(true);
+    if (result.toString) {
+      history.push('/');
+    }
+  };
+
+  const handleError = (error) => {
+    setFailedScan(true);
+  };
+
   return (
     <div>
-      <h2>Camera-page</h2>
+      <Prompt when={failedScan} message={() => `Något gick fel`} />
       <ul>
-        <Link to="/">Tillbaka till förstasidan</Link>
+        <Link to="/">Tillbaka</Link>
       </ul>
+      <h1>Scanna</h1>
+      <QrReader
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>Skane image</p>
     </div>
   );
 };
+
 export default ScanQr;
