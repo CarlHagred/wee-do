8dbb330aa369f525275ff5afdffe2cda96170cdc
CarlHagred <carl.hagred@gmail.com>
2021-10-14 19:52:20 +0200

    Replaced prompt with alert

--- client/src/components/Scanner/ScanCamera.js
+++ client/src/components/Scanner/ScanCamera.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { useHistory, Prompt } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import QrReader from 'react-web-qr-reader';
 
 const ScanCamera = () => {
   const history = useHistory();
-
-  const [failedScan, setFailedScan] = useState(false);
 
   const previewStyle = {
     height: 240,
@@ -13,19 +11,17 @@ const ScanCamera = () => {
   };
 
   const handleScan = (result) => {
-    setFailedScan(true);
     if (result.toString) {
       history.push('/');
     }
   };
 
   const handleError = (error) => {
-    setFailedScan(true);
+    alert(`The code could not be scanned. Error: ${error}`);
   };
 
   return (
     <div>
-      <Prompt when={failedScan} message={() => `Något gick fel`} />
       <QrReader
         style={previewStyle}
         onError={handleError}
