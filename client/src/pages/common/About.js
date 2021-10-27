790321eab5bfb14ff67a56b523b92ec96ba44b47
jgoransson <62560223+jgoransson@users.noreply.github.com>
2021-10-27 11:57:34 +0200

    basic struktur

--- client/src/pages/common/About.js
+++ client/src/pages/common/About.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import Footer from "../../components/common/Footer";
+import Header from "../../components/common/Header";
+import Button from "../../components/common/Button";
+import { Link } from "react-router-dom";
+import PatientTheme from "../../themes/PatientTheme";
 
 const About = () => {
   return (
     <>
-      <h2>About WeeDo</h2>
+    <Header></Header>
+      <strong><h1>Om WeeDo:</h1></strong>
+      <br/>
+      <p>WeeDo är ett samarbete mellan Region Skåne och Malmö Universitetet.</p>
+      <p>Vi är en grupp studenter som går vårt sista år som Informationsarkitekter och Systemutvecklare. 
+      För vårt examensprojekt blev vi utvalda att samarbeta med Region Skåne för att utveckla en webbapplikation för att 
+      brygga avståndet mellan patienter och dess fysioterapefter. Under och efter Covid-19 pandemin är sjukhussängar en bristvara 
+      vilket leder till att vårdpersonalen blir överbelastade samt att nya patienter som behöver vård helt enkelt inte kan få den vård de behöver. 
+      Därför ville Region Skåne göra en webbapplikation för att kunna ge patienter möjlighet till att sköta återhämtning och fysioterapi från sitt eget hem, 
+      samtidigt som man bibehåller en kontinuerlig kontakt med sin fysioterapeft. </p>
+      <br/>
+      <p><strong>Vill du veta mer Malmö Universitet och om de två programmen, följ länkarna nedan:</strong></p>
+      <br/>
+      <a href="https://mau.se/">Malmö Universitet</a>
+      <br/>
+      <a href="https://mau.se/sok-utbildning/program/tgiaa/">Informationsarkitektur</a>
+      <br/>
+      <a href="https://mau.se/sok-utbildning/program/tgsya/">Systemutveckling</a>
+      <br/>
+      <ThemeProvider theme={PatientTheme}>
+        <Button>
+          <Link to="/">Tillbaka</Link>
+        </Button>
+      </ThemeProvider>
+    <Footer></Footer>
     </>
   );
 };
--- client/src/pages/common/Help.js
+++ client/src/pages/common/Help.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import Footer from "../../components/common/Footer";
+import Header from "../../components/common/Header";
+import Button from "../../components/common/Button";
+import { Link } from "react-router-dom";
+import PatientTheme from "../../themes/PatientTheme";
 
 const Help = () => {
   return (
     <>
-      <h2>Hjälp</h2>
+      <ThemeProvider theme={PatientTheme}>
+      <Header></Header>
+        <strong><h2>Hjälp</h2></strong>
+        <p><strong>Jag har tappat bort mitt användarnamn eller QR-kod:</strong></p>
+        <p>
+          Om du har tappat bort ditt användarnamn eller QR-kod måste du komma i kontakt med sjukhuset du besökte när du fick ditt användarnamn eller QR-kod och be om ett nytt. 
+        </p>
+        <br/>
+        <p><strong>Jag kan inte skanna in min QR-kod:</strong></p>
+        <p>Om det inte fungerar att skanna in QR-koden, kontrollera följande:</p>
+        
+        
+        <li>Kontrollera så att din kamera fungerar.</li>
+        <li>Har du accepterat (ja/nej) att webbsidan får använda sig av din kamera?</li> 
+        <li>Om du inte har det, gå till din webbläsare {">"} Inställningar {">"} Integritet och säkerhet {">"} Webbplatsinställningar {">"} Kamera och godkänn användningen av din kamera-enhet.</li>
+        <li>Testa att öka ljusstyrkan på skärmen du skannar din QR-kod på, eller testa att öka ljusstyrkan i rummet.</li>
+        <li>Stödjer din enhet skanning av QR-koder?</li>
+            
+        <br/>
+        <strong>Det fungerar inte att logga in:</strong>
+        <p>Om det inte fungerar att logga in, kontrollera följande:</p>
+        
+          <li>
+            Kontrollera att ditt användarnamn stämmer, 6 tecken. 
+          </li>
+          <li>
+            Kontrollera din nätverksanslutning
+          </li>
+          <li>
+            Om det fortfarande inte fungerar, kontakta Region Skånes Servicedesk
+            Telefon: 077-67 30 000, knappval 2 (IT) följt av 5 (övrigt).
+          </li>
+          <br/>
+          <Button>
+            <Link to="/">Tillbaka</Link>
+          </Button>
+      <Footer></Footer>
+    </ThemeProvider>
     </>
   );
 };
