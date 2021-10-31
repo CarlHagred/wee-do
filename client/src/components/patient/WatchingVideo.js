20a96136e74e9851ceabc227652cb287e684b364
abdulsamisahil <62578384+abdulsamisahil@users.noreply.github.com>
2021-10-31 23:01:29 +0100

    visaövning_klar
    på '/watch' är visa övningen klar, men den körs baserad på titeln av övningen som skulle sedan kopplas till qr-koden,
    detta kan ev ändras till videoId

--- client/src/components/patient/WatchingVideo.js
+++ client/src/components/patient/WatchingVideo.js
@@ -1,28 +1,35 @@
+import React, {useState} from 'react';
 import UserInput from '../common/UserInput';
 import Button from '../common/Button';
 import { getVideoUrl } from "../../api/index";
+import axios from 'axios';
 
-const getInputValue = (event) =>{
-    const input = event.target.value; 
-    return input; 
-}
-const showExercise = (event) => {
-    event.preventDefault(); 
-    const title = getInputValue(event);
-    console.log ("video input title: "+title) 
-    const videoUrl = getVideoUrl(title);
-    return (
-         <div className="watchingExe">
+const WatchExercise = () => {
+    const [title, setTitle] = useState(null);
+    const [videoUrl, setVideoUrl] = useState("");  
+    const getInputValue = (event) =>{
+        setTitle(event.target.value);  
+    }
+    const showExercise = () => {
+        console.log ("video input title: "+title) 
+        axios.get('http://localhost:8000/getVideoUrl', {params: {titel: title}}).then(response => {
+            setVideoUrl(response.data); 
+            console.log('video url: '+ videoUrl); 
+            //const videoUrl = getVideoUrl(title);
+        });  
+    }
+    const renderVideo = () =>{
+    
+        return <div className="watchingExe">     
              <iframe src={videoUrl} frameborder="0" className="youtubePlayer" width="420" height="315" allowFullScreen></iframe>
         </div>
-    ); 
-}
-const WatchExercise = () => {
+    } 
     return (
        <div>
            <div className="titleInput">
                 <UserInput onChange={getInputValue} placeholder="Övningstitel"/>
                 <Button onClick={showExercise}>Spela övning</Button>
+                <iframe src={videoUrl} width="420" height="315" allowFullScreen></iframe>
            </div>
        </div>
     );
--- server/controllers/client/clientEndpoints.js
+++ server/controllers/client/clientEndpoints.js
@@ -4,21 +4,16 @@ export const getVideoUrl = async (req, res) => {
   try {
     //qrkoden som skickas i requesten nås med req.query.qr
     //TODO Lägg till kod för att skicka tillbaka yt-länk från databasen
-    const title = req.query.qr; 
-    
+    const title = req.query.titel; 
+    console.log('Video title: '+title)
     Videos.findOne({övningsTitel: title}, function (err, isVideoInDb){
       if(isVideoInDb != null){
         const videoId = isVideoInDb.övningsId; 
+        console.log('VideoId From Server: ' +videoId)
         let videoUrl = 'http://www.youtube.com/embed/'+videoId; 
-        res.status(200).json({
-          success: true, 
-          url: videoUrl
-        });
+        res.send(videoUrl); 
       }else {
-        res.status(404).json({
-          success:false, 
-          url: 'http://www.youtube.com/embed/404'
-        })
+        res.send('http://www.youtube.com/embed/404')
         console.log(err); 
       }
     })
