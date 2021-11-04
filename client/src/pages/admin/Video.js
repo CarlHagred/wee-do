4da4766fd5930e579cc27e337e238c44cff18ceb
Hanna Ringkvist <ringkvist.hanna@gmail.com>
2021-11-04 16:22:47 +0100

    lagt till en embedded videospelare

--- client/src/pages/admin/Video.js
+++ client/src/pages/admin/Video.js
@@ -8,44 +8,12 @@ import Button from "../../components/common/Button";
 import { getAllVideos } from "../../api";
 import React, { useState, useEffect } from "react";
 
-const StyledTable = styled.table`
-  caption-side: top;
-  border-collapse: separate;
-  border-spacing: 5px;
-  width: 100%;
-  margin: 1em 0;
-  justify-content: left;
-  border-radius: 4px;
-
-  td,
-  th {
-    padding: 5px 10px;
-    border-radius: 4px;
-    font-family: "Roboto", sans-serif;
-    font-size: 1em;
-    text-align: left;
-  }
-
-  tbody tr {
-    :nth-of-type(odd) {
-      background-color: rgb(247, 247, 248, 100%);
-    }
-    :nth-of-type(even) {
-      background-color: rgb(247, 247, 248, 100%);
-    }
-    
-  }
-  thead td {
-    background-color: #c2c2c2;
-    border-radius: 4px;
-    font-size: 1.2em;
-  }
-`;
 
 
 const StyledTitle = styled.p`
   font-weight: bold;
 `;
+
 
 
 const Video = () => {
@@ -64,59 +32,46 @@ const Video = () => {
 
 
   const generateQr = async () => {
-    window.location.href = `/admin/exercise/qrpreview?text=${videoId}&link=www.youtube.com&image=hej`;
+    window.location.href = `/admin/exercise/qrpreview?text=${videos.videoTitle}&link=www.youtube.com&image=hej`;
   };
   
   const deleteVideo = () => {
     console.log("Hej");
   };
 
+  const videoUrl = 'https://www.youtube.com/embed/'+videoId;
 
   return (
     <AdminLayout>
       <ThemeProvider theme={AdminTheme}>
-
-      <StyledTable>
-        <colgroup>
-          <col />
-          <col />
-        </colgroup>
-      
+      <br></br>
+      <p>
+       
         {videos
           .filter((videos) => {
             return videos.videoId.includes(videoId) ? videos : null;
           })
           .map((videos) => (
-            <tbody>
-              
-              
-              <tr>
-                <td>
-                  <StyledTitle>
-                  
+            
+            <StyledTitle>
+                  <p align="center">
                     {videos.videoTitle}
-                  
+                  </p>
                   </StyledTitle>
                 
-                  <br></br>
-                  <img src={videos.thumbnail} alt="profile pic" width="250px" height="200px" />
-                  <br></br>
-                  <br></br>
-                  Antal visningar: {videos.__v}
-                </td>
-              </tr>
-              <br></br>
-              <br></br>
-              <br></br>
-              
-            </tbody>
           ))}
-      </StyledTable>
+          <br></br>
+          <br></br>
+          <br></br>
+      </p>
 
-      
+      <p align="center"><iframe width="560" height="315" src={videoUrl} frameborder="0" allowfullscreen></iframe></p>
+
         <Button onClick={generateQr} icon="qrcode">Generera QR-kod</Button>
       
         <Button onClick={deleteVideo} icon="trash">Radera</Button>
+
+        
       </ThemeProvider>
     </AdminLayout>
   );
