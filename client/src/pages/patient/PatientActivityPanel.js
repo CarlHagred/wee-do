6616190155e9d78cba0352f6e08fdc5c70dcdf9f
Josefine Klintberg <jool@snel.se>
2021-11-04 15:52:31 +0100

    Styling update
    Changed RS-Logo in footer to svg. Hero-image added to PatientLogin-view. Changed margin in buttons. Removed PlaceHolder.js

--- client/src/pages/patient/PatientActivityPanel.js
+++ client/src/pages/patient/PatientActivityPanel.js
@@ -5,28 +5,31 @@ import PatientLayout from "../../components/patient/PatientLayout";
 import { ThemeProvider } from "styled-components";
 import { Link } from "react-router-dom";
 import { customDialogPatient } from "../../components/common/Confirmation";
-import PlaceHolder from "../../components/common/PlaceHolder";
 
-  
 const PatientActivityPanel = () => {
   return (
-    <>  
-        <PatientLayout >
-        <PlaceHolder/>
-        <ThemeProvider theme={PatientTheme}>
-            <Link to="/QrScanner">
-                <Button size="lg" icon="qrcode">Scanna QR-kod</Button>
-            </Link>
-            <Link to="/">
-                <Button size="lg" icon="statistics">Se statistik</Button>
-            </Link>
-            <Button onClick={customDialogPatient} neutral icon="exit" color="#8a8883">
-                Logga ut
-            </Button>
-             
-        </ThemeProvider>
-        </PatientLayout>    
-    </>
-);
+    <PatientLayout>
+      <ThemeProvider theme={PatientTheme}>
+        <Link to="/QrScanner">
+          <Button size="lg" icon="qrcode">
+            Scanna QR-kod
+          </Button>
+        </Link>
+        <Link to="/">
+          <Button size="lg" icon="statistics">
+            Se statistik
+          </Button>
+        </Link>
+        <Button
+          onClick={customDialogPatient}
+          neutral
+          icon="exit"
+          color="#8a8883"
+        >
+          Logga ut
+        </Button>
+      </ThemeProvider>
+    </PatientLayout>
+  );
 };
 export default PatientActivityPanel;
