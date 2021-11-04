2ab7aa51d96dbaa06f1eb7811cf5ddba48feda89
Josefine Klintberg <jool@snel.se>
2021-11-04 19:37:13 +0100

    Banner added and theme altered
    Under construction: Color Scheme of Admin Theme, test with AdminHeroBanner

--- client/src/pages/admin/AdminPanel.js
+++ client/src/pages/admin/AdminPanel.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
 import AdminLayout from "../../components/admin/AdminLayout";
 import { getAdminSession } from "../../api";
+import hero from "../../components/images/AdminHeroBanner.png";
+
+const StyledHero = styled.div`
+  margin: -1rem -1rem 2rem -1rem;
+`;
 
 const AdminPanel = () => {
   const [admin, setAdmin] = useState("");
@@ -14,6 +21,10 @@ const AdminPanel = () => {
   });
   return (
     <AdminLayout>
+      <StyledHero>
+        <img src={hero} alt="Activity Banner" width="100%" />
+      </StyledHero>
+
       <h2 style={{ textAlign: "center" }}>Vällkommen {admin.username}</h2>
       <br />
     </AdminLayout>
