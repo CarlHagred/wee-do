d909d6381a7f625077b314c620fecf43d6299cc8
Josefine Klintberg <jool@snel.se>
2021-11-08 17:02:19 +0100

    Update PatientLogin.js

--- client/src/pages/patient/PatientLogin.js
+++ client/src/pages/patient/PatientLogin.js
@@ -6,16 +6,55 @@ import LoginPatient from "../../components/Login/LoginPatient";
 import hero from "../../components/images/patient_hero.png";
 
 const StyledHero = styled.div`
-  margin: -1rem -1rem 2rem -1rem;
+  background-image: url(${hero});
+  margin: -1rem -1rem 0 -1rem;
+  background-image: url(${hero});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  @media (min-width: 768px) {
+    margin: 0;
+  }
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 2em;
+  margin: 40px 0;
+  text-align: center;
+`;
+
+const LoginWrapper = styled.div`
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    margin-top: 10rem;
+    background-color: white;
+    border: 1px solid #bfc1bf;
+    height: 420px;
+    width: 800px;
+  }
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    margin: 0 20px;
+    width: 400px;
+  }
 `;
 
 const PatientLogin = () => {
   return (
     <PatientLayout>
-      <StyledHero>
-        <img src={hero} alt="WeeDo" width="100%" />
-      </StyledHero>
-      <LoginPatient />
+      <LoginWrapper>
+        <StyledHero />
+        <LoginContainer>
+          <StyledHeader>Logga in</StyledHeader>
+          <LoginPatient />
+        </LoginContainer>
+      </LoginWrapper>
     </PatientLayout>
   );
 };
