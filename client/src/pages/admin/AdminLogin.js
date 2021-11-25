01951c1d184875a012820b400a96fef2174297f1
jgoransson <62560223+jgoransson@users.noreply.github.com>
2021-11-25 19:48:05 +0100

    Update AdminLogin.js

--- client/src/pages/admin/AdminLogin.js
+++ client/src/pages/admin/AdminLogin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 
