3911ea07b6ce16c8dfe6b0fb93528d737e8783f3
Josefine Klintberg <jool@snel.se>
2021-11-25 10:54:31 +0100

    Patient Navbar update
    Fixed link to mainpage instead of acitivitypanel when clicking on logo

--- client/src/components/patient/PatientNavbar.js
+++ client/src/components/patient/PatientNavbar.js
@@ -12,287 +12,251 @@ import { customDialogPatient } from "../common/Confirmation";
 import { getSession } from "../../api";
 
 const StyledIcon = styled(Icon)`
-    margin-right: 10px;
+  margin-right: 10px;
 `;
 
 const StyledMobileNav = styled.div`
-    display: none;
-    background: ${(props) => props.theme.palette.brand};
-    max-height: ${(p) => (p.open ? "500px" : 0)};
-    overflow: hidden;
-    transition: 250ms max-height ease-in-out;
-    @media (max-width: 768px) {
-        display: block;
-    }
+  display: none;
+  background: ${(props) => props.theme.palette.brand};
+  max-height: ${(p) => (p.open ? "500px" : 0)};
+  overflow: hidden;
+  transition: 250ms max-height ease-in-out;
+  @media (max-width: 768px) {
+    display: block;
+  }
 `;
 
 const NavbarMenu = styled.nav`
-    display: flex;
-    height: 70px;
-    list-style-type: none;
-    background: ${(props) => props.theme.palette.brand};
+  display: flex;
+  height: 70px;
+  list-style-type: none;
+  background: ${(props) => props.theme.palette.brand};
 `;
 
 const NavbarItem = styled(NavLink)`
-    display: flex;
-    align-items: center;
-    padding: 0 20px;
-    color: white;
-    height: 100%;
-    font-size: 1em;
-    &:hover,
-    &.active {
-        background-color: ${(props) => props.theme.palette.hover};
-    }
-    @media (max-width: 768px) {
-        display: none;
-    }
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  color: white;
+  height: 100%;
+  font-size: 1em;
+  &:hover,
+  &.active {
+    background-color: ${(props) => props.theme.palette.hover};
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const PatientName = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  height: 100%;
+  font-size: 1em;
+  margin-left: auto;
+  @media (max-width: 768px) {
     display: flex;
-    align-items: center;
-    color: white;
-    height: 100%;
-    font-size: 1em;
-    margin-left: auto;
-    @media (max-width: 768px) {
-        display: flex;
-        justify-content: right;
-        margin-right: 10px;
-        font-size: 1.2em;
-    };
+    justify-content: right;
+    margin-right: 10px;
+    font-size: 1.2em;
+  }
 
-    @media (max-width: 410px) {
-        margin-right: 2%;
-        
-    };
-    
-`
+  @media (max-width: 410px) {
+    margin-right: 2%;
+  } ;
+`;
 
 const NavbarItemLogout = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 0 20px;
-    color: white;
-    height: 100%;
-    font-size: 1em;
-    ${(p) => p.last && `margin-left: 10px`};
-    &:hover,
-    &.active {
-        background-color: ${(props) => props.theme.palette.hover};
-    }
-    @media (max-width: 768px) {
-        display: none;
-    }
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  color: white;
+  height: 100%;
+  font-size: 1em;
+  ${(p) => p.last && `margin-left: 10px`};
+  &:hover,
+  &.active {
+    background-color: ${(props) => props.theme.palette.hover};
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavbarItemBurger = styled(NavLink)`
-    display: flex;
-    padding: 20px;
-    align-items: center;
-    color: white;
-    height: 100%;
-    font-size: 1.2em;
-    &:hover,
-    &.active {
-        background-color: ${(props) => props.theme.palette.hover};
-    }
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  color: white;
+  height: 100%;
+  font-size: 1.2em;
+  &:hover,
+  &.active {
+    background-color: ${(props) => props.theme.palette.hover};
+  }
 `;
 
 const NavBarItemBurgerLogout = styled.div`
-    display: flex;
-    padding: 20px;
-    align-items: center;
-    color: white;
-    height: 100%;
-    font-size: 1.2em;
-    &:hover {
-        background-color: ${(props) => props.theme.palette.hover};
-    }
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  color: white;
+  height: 100%;
+  font-size: 1.2em;
+  &:hover {
+    background-color: ${(props) => props.theme.palette.hover};
+  }
 `;
 
 const LogOut = styled(NavBarItemBurgerLogout)`
-    justify-content: center;
-    font-size: 1.3em;
-    background-color: ${(props) => props.theme.palette.hamburger};
+  justify-content: center;
+  font-size: 1.3em;
+  background-color: ${(props) => props.theme.palette.hamburger};
 `;
 
 const NavbarBurger = styled.button`
-    display: none;
-    @media (max-width: 768px) {
-        display: block;
-        padding: 0 20px;
-    }
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+    padding: 0 20px;
+  }
 `;
 
 const NavbarLogo = styled(Link)`
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  color: white;
+  height: 100%;
+  font-size: 1.8em;
+  @media (max-width: 768px) {
     display: flex;
-    align-items: center;
-    padding: 0 20px;
-    color: white;
-    height: 100%;
-    font-size: 1.8em;
-    @media (max-width: 768px) {
-        display: flex;
-        align-self: center;
-        flex: 1;
-        justify-content: center;
-        padding: 0 70px 0 0;
-        margin-left: 70px;
-    }
-    
-    @media (max-width: 410px) {
-        margin: 0;
-        padding: 0;
-    }
+    align-self: center;
+    flex: 1;
+    justify-content: center;
+    padding: 0 70px 0 0;
+    margin-left: 70px;
+  }
+
+  @media (max-width: 410px) {
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 const StyledDivider = styled.hr`
-    align-content: center;
-    width: 90%;
-    margin: 0 auto;
-    border: 1px solid;
-    border-color: ${(props) => props.theme.palette.hamburger};
+  align-content: center;
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid;
+  border-color: ${(props) => props.theme.palette.hamburger};
 `;
 
 const Navbar = () => {
-    /* === PATIENT SESSION === */
-    const [patient, setPatient] = useState("");
-    useEffect(() => {
-        const fetchData = async () => {
-            const fetchedPatientSession = await getSession();
-            setPatient(fetchedPatientSession.data);
-        };
-        fetchData();
-    }, []);
+  /* === PATIENT SESSION === */
+  const [patient, setPatient] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedPatientSession = await getSession();
+      setPatient(fetchedPatientSession.data);
+    };
+    fetchData();
+  }, []);
 
-    /* === NAVBAR ===*/
-    const closeMenu = () => setOpen(false);
-    const [open, setOpen] = useState(false);
-    const url = "http://localhost:3000/activitypanel";
+  /* === NAVBAR ===*/
+  const closeMenu = () => setOpen(false);
+  const [open, setOpen] = useState(false);
+  const url = "http://localhost:3000/activitypanel";
 
-    if (window.location.href !== url) {
-        return (
-            <>
-                <NavbarMenu theme={PatientTheme}>
-                    <NavbarBurger>
-                        <Hamburger
-                            toggled={open}
-                            toggle={setOpen}
-                            rounded
-                            color="white"
-                        />
-                    </NavbarBurger>
+  if (window.location.href !== url) {
+    return (
+      <>
+        <NavbarMenu theme={PatientTheme}>
+          <NavbarBurger>
+            <Hamburger toggled={open} toggle={setOpen} rounded color="white" />
+          </NavbarBurger>
 
-                    <NavbarLogo to="/activitypanel">
-                        <WdLogo
-                            width="4em"
-                            height="4em"
-                            fill="#FFFFFF"
-                            alt="WeeDo Logo"
-                        />
-                    </NavbarLogo>
-    
-                    <NavbarItem to="/QrScanner">Scanna övning</NavbarItem>
-                    <NavbarItem to="/statistics">Se statistik</NavbarItem>
+          <NavbarLogo to="/mainpage">
+            <WdLogo width="4em" height="4em" fill="#FFFFFF" alt="WeeDo Logo" />
+          </NavbarLogo>
 
-                    <PatientName>
-                        {patient.name}
-                    </PatientName>
+          <NavbarItem to="/QrScanner">Scanna övning</NavbarItem>
+          <NavbarItem to="/statistics">Se statistik</NavbarItem>
 
-                    <NavbarItemLogout
-                        isActive={() => false}
-                        onClick={customDialogPatient}
-                        last="true"
-                    >
-                        Logga ut
-                    </NavbarItemLogout>
-                </NavbarMenu>
+          <PatientName>{patient.name}</PatientName>
 
-                <StyledMobileNav open={open}>
-                    <NavbarItemBurger to="/QrScanner" onClick={closeMenu}>
-                        <StyledIcon size="1.5em" name="qrcode" /> Scanna övning
-                    </NavbarItemBurger>
-                    <StyledDivider />
+          <NavbarItemLogout
+            isActive={() => false}
+            onClick={customDialogPatient}
+            last="true"
+          >
+            Logga ut
+          </NavbarItemLogout>
+        </NavbarMenu>
 
-                    <NavbarItemBurger to="/statistics" onClick={closeMenu}>
-                        <StyledIcon size="1.5em" name="statistics" /> Se
-                        statistik
-                    </NavbarItemBurger>
-                    <StyledDivider />
-                    <NavbarItemBurger to="/help" onClick={closeMenu}>
-                        Hjälp
-                    </NavbarItemBurger>
-                    <StyledDivider />
+        <StyledMobileNav open={open}>
+          <NavbarItemBurger to="/QrScanner" onClick={closeMenu}>
+            <StyledIcon size="1.5em" name="qrcode" /> Scanna övning
+          </NavbarItemBurger>
+          <StyledDivider />
 
-                    <NavbarItemBurger to="/about" onClick={closeMenu}>
-                        Om WeeDo
-                    </NavbarItemBurger>
+          <NavbarItemBurger to="/statistics" onClick={closeMenu}>
+            <StyledIcon size="1.5em" name="statistics" /> Se statistik
+          </NavbarItemBurger>
+          <StyledDivider />
+          <NavbarItemBurger to="/help" onClick={closeMenu}>
+            Hjälp
+          </NavbarItemBurger>
+          <StyledDivider />
 
-                    <LogOut
-                        to="/"
-                        isActive={() => false}
-                        onClick={customDialogPatient}
-                    >
-                         Logga ut
-                    </LogOut>
-                </StyledMobileNav>
-            </>
-        );
-    }
-    if (window.location.href === url) {
-        return (
-            <>
-                <NavbarMenu theme={PatientTheme}>
-                    <NavbarBurger>
-                        <Hamburger
-                            toggled={open}
-                            toggle={setOpen}
-                            rounded
-                            color="white"
-                        />
-                    </NavbarBurger>
+          <NavbarItemBurger to="/about" onClick={closeMenu}>
+            Om WeeDo
+          </NavbarItemBurger>
 
-                    <NavbarLogo to="/activitypanel">
-                        <WdLogo
-                            width="4em"
-                            height="4em"
-                            fill="#FFFFFF"
-                            alt="WeeDo Logo"
-                        />
-                    </NavbarLogo>
+          <LogOut to="/" isActive={() => false} onClick={customDialogPatient}>
+            Logga ut
+          </LogOut>
+        </StyledMobileNav>
+      </>
+    );
+  }
+  if (window.location.href === url) {
+    return (
+      <>
+        <NavbarMenu theme={PatientTheme}>
+          <NavbarBurger>
+            <Hamburger toggled={open} toggle={setOpen} rounded color="white" />
+          </NavbarBurger>
 
-                    <PatientName>
-                        {patient.name}
-                    </PatientName>
+          <NavbarLogo to="/activitypanel">
+            <WdLogo width="4em" height="4em" fill="#FFFFFF" alt="WeeDo Logo" />
+          </NavbarLogo>
 
-                    <NavbarItemLogout
-                        isActive={() => false}
-                        onClick={customDialogPatient}
-                        last="true"
-                    >
-                        Logga ut
-                    </NavbarItemLogout>
-                    
-                </NavbarMenu>
+          <PatientName>{patient.name}</PatientName>
 
-                <StyledMobileNav open={open}>
-                    <StyledDivider />
-                    <NavbarItemBurger to="/help" onClick={closeMenu}>
-                        Hjälp
-                    </NavbarItemBurger>
-                    <LogOut
-                        to="/"
-                        isActive={() => false}
-                        onClick={customDialogPatient}
-                    >
-                        Logga ut
-                    </LogOut>
-                </StyledMobileNav>
-            </>
-        );
-    }
+          <NavbarItemLogout
+            isActive={() => false}
+            onClick={customDialogPatient}
+            last="true"
+          >
+            Logga ut
+          </NavbarItemLogout>
+        </NavbarMenu>
+
+        <StyledMobileNav open={open}>
+          <StyledDivider />
+          <NavbarItemBurger to="/help" onClick={closeMenu}>
+            Hjälp
+          </NavbarItemBurger>
+          <LogOut to="/" isActive={() => false} onClick={customDialogPatient}>
+            Logga ut
+          </LogOut>
+        </StyledMobileNav>
+      </>
+    );
+  }
 };
 
 export default Navbar;
