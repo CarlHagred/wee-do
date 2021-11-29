ce193a2a810bb674f638b94677c1c0e28dc0d01d
Josefine Klintberg <jool@snel.se>
2021-11-29 22:02:41 +0100

    AdminPanel update
    PatientActivityPanel.js is now PatientPanel.js
    AdminPanel.js now uses the same PanelButton-component as PatientPanel.js

--- client/src/pages/admin/AdminPanel.js
+++ client/src/pages/admin/AdminPanel.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-
-import AdminTheme from "../../themes/AdminTheme";
 
 import { getAdminSession } from "../../api";
 
 import AdminLayout from "../../components/admin/AdminLayout";
+import PanelButton from "../../components/common/PanelButton";
 import hero from "../../components/images/AdminHeroBanner.png";
-import Icon from "../../components/common/Icons";
-import ContentContainer from "../../components/common/ContentContainer";
 
 const StyledHero = styled.div`
   width: 100vw;
@@ -32,38 +28,19 @@ const StyledHeroHeader = styled.h1`
   font-size: 3em;
 `;
 
-const PanelMenu = styled.nav`
+const PanelMenuWrapper = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  list-style-type: none;
-  margin-top: 20px;
   justify-content: center;
 `;
 
-const PanelContainer = styled(NavLink)`
+const PanelMenu = styled.nav`
   display: flex;
-  flex-direction: column;
-  min-height: 135px;
-  min-width: 238px;
-  background: ${(props) => props.theme.palette.brand};
-  &:hover {
-    background-color: ${(props) => props.theme.palette.hover};
-  }
-`;
-
-const StyledPanelText = styled.p`
-  align-self: flex-start;
-  color: white;
-  font-size: 1.3em;
-  margin-left: 20px;
-`;
-
-const StyledPanelIcon = styled(Icon)`
-  align-self: flex-end;
-  color: white;
-  font-size: 1.3em;
-  margin: 20px 20px 10px 0;
+  flex-wrap: wrap;
+  gap: 2em;
+  align-items: center;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  justify-content: center;
 `;
 
 const AdminPanel = () => {
@@ -80,39 +57,33 @@ const AdminPanel = () => {
       <StyledHero>
         <StyledHeroHeader>Välkommen {admin.username}</StyledHeroHeader>
       </StyledHero>
-      <ContentContainer>
-        <PanelMenu theme={AdminTheme}>
-          <PanelContainer to="/admin/register/patient">
-            <StyledPanelIcon name="add_user" size="3em" fill="white" />
-            <StyledPanelText>Registrera patient</StyledPanelText>
-          </PanelContainer>
 
-          <PanelContainer to="/admin/search/patient">
-            <StyledPanelIcon
-              name="search_patient"
-              width="62"
-              height="62"
-              fill="white"
-            />
-            <StyledPanelText>Sök patient</StyledPanelText>
-          </PanelContainer>
-
-          <PanelContainer to="/admin/register/exercise">
-            <StyledPanelIcon name="upload" size="3em" fill="white" />
-            <StyledPanelText>Ladda upp övning</StyledPanelText>
-          </PanelContainer>
-
-          <PanelContainer to="/admin/search/exercise">
-            <StyledPanelIcon
-              name="search_exercise"
-              width="62"
-              height="62"
-              fill="white"
-            />
-            <StyledPanelText>Sök övning</StyledPanelText>
-          </PanelContainer>
+      <PanelMenuWrapper>
+        <PanelMenu>
+          <PanelButton to="/admin/register/patient" icon="add_user" size="3em">
+            Registrera patient
+          </PanelButton>
+          <PanelButton
+            to="/admin/search/patient"
+            icon="search_patient"
+            width="62"
+            height="62"
+          >
+            Sök patient
+          </PanelButton>
+          <PanelButton to="/admin/register/exercise" icon="upload" size="3em">
+            Ladda upp övning
+          </PanelButton>
+          <PanelButton
+            to="/admin/search/exercise"
+            icon="search_exercise"
+            width="62"
+            height="62"
+          >
+            Sök övning
+          </PanelButton>
         </PanelMenu>
-      </ContentContainer>
+      </PanelMenuWrapper>
     </AdminLayout>
   );
 };
