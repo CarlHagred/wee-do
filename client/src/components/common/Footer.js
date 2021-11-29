51fb8ea1ba0c55f53a9bf34e1a5e4845adf78800
Josefine Klintberg <jool@snel.se>
2021-11-30 00:03:25 +0100

    Update Footer.js

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -3,111 +3,96 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 import WdLogo from "../images/WdLogo";
-import MauLogo from "../images/MauLogo";
 
 const StyledFooter = styled.footer`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    background: ${(props) => props.theme.palette.brand};
-    color: white;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  background: ${(props) => props.theme.palette.brand};
+  color: white;
 
-    padding: 20px;
+  padding: 20px;
 
-    @media (min-width: 768px) {
-        padding: 20px 80px 30px 80px;
-    }
+  @media (min-width: 768px) {
+    padding: 20px 80px 30px 80px;
+  }
 `;
 
 const TopContainer = styled.div`
-    flex: 1;
+  flex: 1;
 `;
 
 const BotttomContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 40px;
-    @media (min-width: 400px) {
-    }
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 40px;
+  @media (min-width: 400px) {
+  }
 `;
 
 const AboutContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    max-width: 280px;
+  display: flex;
+  flex-direction: column;
+  max-width: 280px;
 `;
 
 const StyledParagraph = styled.p`
-    font-style: italic;
-    padding: 5px 0 10px 5px;
+  font-style: italic;
+  padding: 5px 0 10px 5px;
 `;
 
-const LogoContainer = styled.div``;
-
 const NavMenu = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 3em;
-    @media (min-width: 550px) {
-        ${(p) => p.last && `margin-left: auto`};
-    }
+  display: flex;
+  flex-direction: row;
+  gap: 3em;
+  @media (min-width: 550px) {
+    ${(p) => p.last && `margin-left: auto`};
+  }
 `;
 
 const NavContainer = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const NavHeader = styled.h1`
-    font-weight: bold;
-    font-size: 1.1em;
+  font-weight: bold;
+  font-size: 1.1em;
 `;
 
 const NavItem = styled(NavLink)`
-    margin-top: 5px;
-    &:hover {
-        text-decoration: underline;
-    }
+  margin-top: 5px;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const Footer = () => {
-    return (
-        <StyledFooter>
-            <TopContainer>
-                <WdLogo
-                    width="128px"
-                    height="45px"
-                    fill="white"
-                    alt="WeeDo logotyp"
-                />
-            </TopContainer>
+  return (
+    <StyledFooter>
+      <TopContainer>
+        <WdLogo width="128px" height="45px" fill="white" alt="WeeDo logotyp" />
+      </TopContainer>
 
-            <BotttomContainer>
-                <AboutContainer>
-                    <StyledParagraph>
-                        WeeDo är ett samarbete mellan sjukgymnaster på Mobilt Sjukhus Team  och studenter på Malmö Universitet.
-                    </StyledParagraph>
-                    <LogoContainer>
-                        <MauLogo
-                            width="52px"
-                            height="45px"
-                            fill="white"
-                            alt="Malmö Universitets logotyp"
-                        />
-                    </LogoContainer>
-                </AboutContainer>
-                <NavMenu last="true">
-                    <NavContainer>
-                        <NavHeader>Information</NavHeader>
+      <BotttomContainer>
+        <AboutContainer>
+          <StyledParagraph>
+            WeeDo är ett samarbete mellan sjukgymnaster på Mobilt Sjukhus Team
+            och studenter på Malmö Universitet.
+          </StyledParagraph>
+        </AboutContainer>
+        <NavMenu last="true">
+          <NavContainer>
+            <NavHeader>Information</NavHeader>
 
-                        <NavItem to="/help">Hjälp</NavItem>
-                        <NavItem to="/about">Om WeeDo</NavItem>
-                    </NavContainer>
-                </NavMenu>
-            </BotttomContainer>
-        </StyledFooter>
-    );
+            <NavItem to="/help">Hjälp</NavItem>
+            <NavItem to="/about">Om WeeDo</NavItem>
+          </NavContainer>
+        </NavMenu>
+      </BotttomContainer>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
