abd2aa0d98c099cbcf628c0ce27c34c68f730202
JohanFrom <gouldsilver@gmail.com>
2021-12-01 14:42:30 +0100

    Styled success och error
    Stylat success sidan och error sidan, och tog bort en bracket från watching video som var fel

--- client/src/pages/admin/UploadError.js
+++ client/src/pages/admin/UploadError.js
@@ -1,18 +1,29 @@
 import AdminLayout from "../../components/admin/AdminLayout";
 import ContentContainer from "../../components/common/ContentContainer";
+import styled from "styled-components";
+import Button from "../../components/common/Button";
+import AdminTheme from "../../themes/AdminTheme";
+
+const ErrorMessage = styled.h1`
+    display: flex;
+    justify-content: center;
+    font-size: 25px;
+    margin-top: 100px;
+    margin-bottom: 30px;
+`
+
+const goToUploadPage = () => {
+    //window.location = "/admin/register/exercise";
+    window.location = "/adminpanel"
+}
 
 const UploadError = () => {
     return (
         <AdminLayout>
             <ContentContainer>
                 <div>
-                    <br />
-                    <br />
-                    <h1>Ett fel inträffade när videon skulle laddas upp!</h1>
-                    <br />
-                    <ul>
-                        <br />
-                    </ul>
+                    <ErrorMessage>Ett fel inträffade när videon skulle laddas upp!</ErrorMessage>
+                    <Button theme={AdminTheme} onClick={goToUploadPage}>Tillbaka till startsidan</Button>
                 </div>
             </ContentContainer>
         </AdminLayout>
