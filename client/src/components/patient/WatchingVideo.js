81a8bafbd49757c62a4f9f4de62628f70aba7521
Abdul Sami <sami.kdr@live.se>
2021-12-03 13:18:16 +0100

    fixed button utförtövningen in watchingVideo.js
    Removed unnecessary code lines and states.

--- client/src/components/patient/WatchingVideo.js
+++ client/src/components/patient/WatchingVideo.js
@@ -10,10 +10,7 @@ import PatientLayout from "./PatientLayout";
 import ReactPlayer from "../common/ReactPlayer";
 import styled from "styled-components";
 
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 const H2 = styled.h2`
   font-size: 1.5em;
   padding: 10px;
@@ -25,6 +22,12 @@ const P = styled.p`
   color: gray;
   padding: 10px;
   text-align: center;
+`;
+
+const Container = styled.div`
+  display: flex; 
+  justify-content: center; 
+  align-items: center
 `;
 
 const WatchExercise = () => {
@@ -39,20 +42,14 @@ const WatchExercise = () => {
   const [watchedVideo, setWatchedVideo] = useState(false);
   const [active, setActive] = useState(true);
   const [showActive, setShowActive] = useState(false);
-  // const [watchedVideo, setWatchedVideo] = useState(false);
+ 
 
   const [title, setTitle] = useState(null);
   const [description, setDescription] = useState(null);
   const [isTitleAndDescFetched, setIsTitleAndDescFetched] = useState(false);
 
-  const [buttonInnerText, setButtonInnerText] = useState(
-    "Jag har utfört övningen"
-  );
-  const [buttonBackground, setButtonBackground] = useState("red");
   const [watchedButtonDisabled, setWatchedButtonDisabled] = useState(true);
-  const [textAboutBtn, setTextAboutBtn] = useState(
-    "Knappen kan tryckas på först efter när du har sett klart övningen"
-  );
+ 
   useEffect(() => {
     const fetchData = async () => {
       const fetchedSession = await getSession();
@@ -71,7 +68,7 @@ const WatchExercise = () => {
       setIsTitleAndDescFetched(true);
     };
     titleAndDesc(videoId);
-  }, []);
+  }, [videoId]);
 
   const handleEvent = async () => {
     const handleClick = await postWatchedVideo(patientName, videoId, active);
@@ -82,8 +79,6 @@ const WatchExercise = () => {
       ? setShowActive(true)
       : setShowActive(false);
     await postWatchedVideo(patientName, videoId);
-    setButtonBackground("green");
-    setButtonInnerText("Bra jobbat...!");
   };
 
   const playerProps = {
@@ -91,11 +86,6 @@ const WatchExercise = () => {
     playing: true,
     onEnded: () => {
       setWatchedButtonDisabled(false);
-      setTextAboutBtn("Nu går det bra att klicka på knappen");
-      setTimeout(() => {
-        setWatchedButtonDisabled(true);
-        setTextAboutBtn("Knappen kan tryckas på endast EN gång");
-      }, 3000);
     },
   };
 
@@ -107,28 +97,19 @@ const WatchExercise = () => {
           <H2>{title}</H2>
           <P>{description}</P>
         </div>
-      )}
-      <div className="btn-Watched-Video">
-        <Button
-          disabled={watchedButtonDisabled}
-          onClick={handleEvent}
-          style={{ margin: "1em 0em", background: `${buttonBackground}` }}
-        >
-          {buttonInnerText}
-        </Button>
-      </div>
-      {watchedVideo ? (
-        <p>Bra jobbat!</p>
-      ) : (
-        <button onClick={handleEvent}>
-          Jag har tittat på övningen och gjort den
-        </button>
-      )}
-      {showActive ? (
-        <p>Jättebra jobbat, du är inaktiv så din statistik sparas inte. </p>
-      ) : null}
-
-      <P>{textAboutBtn}</P>
+      )} <Container>
+          {watchedVideo ? (
+            <Button disabled={true} style={{background: "green"}}>Bra jobbat!</Button>
+          ) : (
+            <Button disabled={watchedButtonDisabled} onClick={handleEvent} style={{position: 'relative'}}>
+              "Jag har utfört övningen"
+            </Button>
+          )}
+          {showActive ? (
+            <P>Jättebra jobbat, du är inaktiv så din statistik sparas inte. </P>
+          ) : null
+          }
+        </Container>
     </PatientLayout>
   );
 };
