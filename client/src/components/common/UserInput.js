b083f93055844f99b7ce73ed3d55b2871f3d9547
viktorpolak98 <61971346+viktorpolak98@users.noreply.github.com>
2021-12-15 18:30:46 +0100

    user input changes

--- client/src/components/common/UserInput.js
+++ client/src/components/common/UserInput.js
@@ -17,7 +17,24 @@ const StyledInput = styled.input`
   text-align: left;
 `;
 
-const UserInput = (props) => {
+const StyledInputPatien = styled(StyledInput)`
+  height: 4em;
+  font-size: 1.7em;
+`;
+
+export const PatientInput = (props) => {
+  return (
+    <StyledInputPatien
+      id="input"
+      type="text"
+      placeholder="Användarnamn..."
+      onChange={props.onChange}
+      {...props}
+    />
+  );
+};
+
+export const UserInput = (props) => {
   return (
     <StyledInput
       id="input"
@@ -29,4 +46,4 @@ const UserInput = (props) => {
   );
 };
 
-export default UserInput;
+export default UserInput + PatientInput;
--- client/src/components/patient/LoginPatient.js
+++ client/src/components/patient/LoginPatient.js
@@ -6,7 +6,7 @@ import { loginPatient } from "../../api";
 import PatientTheme from "../../themes/PatientTheme";
 
 import Button from "../common/Button";
-import UserInput from "../common/UserInput";
+import { PatientInput } from "../common/UserInput";
 
 const LoginContainer = styled.div`
   width: 100%;
@@ -41,14 +41,14 @@ const LoginPatient = () => {
   return (
     <LoginContainer>
       <span id="patientError" />
-      <UserInput
+      <PatientInput
         theme={PatientTheme}
         type="text"
         name="name"
         id="loginPatient"
         onKeyDown={listener}
         onChange={(e) => setLoginName(e.target.value)}
-        placeholder="Skriv användarnamn här..."
+        placeholder="Skriv användarnamn här"
       />
       <Button width="wide" onClick={handleSubmit}>
         Logga in
