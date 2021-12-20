9d721b79ed80a48ab8625e37415d5df11dd6b685
Josefine Klintberg <jool@snel.se>
2021-12-20 13:52:22 +0100

    Update ToDo.js
    Det tar sig

--- client/src/pages/patient/ToDo.js
+++ client/src/pages/patient/ToDo.js
@@ -5,16 +5,93 @@ import { getSession, getMyVideos } from "../../api/index";
 import PatientLayout from "../../components/patient/PatientLayout";
 import styled from "styled-components";
 
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1000px;
+  align-content: center;
+  margin: 3em auto 0 auto;
+  @media (max-width: 1085px) {
+    max-width: 660px;
+  }
+  @media (max-width: 768px) {
+    max-width: 328px;
+    min-width: 250px;
+  }
+`;
+
+const VideoContainer = styled.div`
+  height: 260px;
+`;
+
+const StyledThumbnail = styled.img`
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: 0.5s ease;
+  backface-visibility: hidden;
+`;
+
+const HoverContainer = styled.div`
+  transition: 0.5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+const ThumbnailContainer = styled.div`
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.9);
+  :hover ${StyledThumbnail} {
+    opacity: 0.3;
+  }
+  :hover ${HoverContainer} {
+    opacity: 1;
+  }
+`;
+
+const StyledHoverText = styled.div`
+  font-size: 32px;
+  padding: 16px 32px;
+  color: white;
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 1.2em;
+  padding-bottom: 1em;
+  font-weight: 600;
+`;
+
+const SearchResultContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1.2em;
+`;
+
+const StyledVideoTitle = styled.h1`
+  font-weight: bold;
+  width: 280px;
+  padding-top: 10px;
+  padding-bottom: 5px;
+`;
+
+const StyledVideoText = styled.p`
+  font-size: 0.9em;
+  color: darkgrey;
+`;
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 3rem;
   flex-direction: column;
   text-align: center;
-`;
-
-const StyledHeader = styled.h1`
-  font-size: 3em;
 `;
 
 const ToDo = () => {
@@ -42,27 +119,36 @@ const ToDo = () => {
 
   return (
     <PatientLayout>
-      <Wrapper>
-        <StyledHeader>Mina övningar</StyledHeader>
-        <ul>
-          {myVideos?.data?.data.map((video) => {
-            return (
-              <li>
+      <StyledContentContainer>
+        <Wrapper>
+          <StyledHeader>Mina övningar</StyledHeader>
+          <SearchResultContainer>
+            {myVideos?.data?.data.map((video) => {
+              return (
                 <Link
                   to={`/watch?title=http://www.youtube.com/embed/${video.vidId}`}
                   key={video.vidId}
                 >
-                  <img
-                    src={`https://img.youtube.com/vi/${video.vidId}/mqdefault.jpg`}
-                  />
-                  {video.vidTitle}
-                  Du har gjort denna: {video.amountOfTimes} gånger
+                  <VideoContainer>
+                    <ThumbnailContainer>
+                      <img
+                        src={`https://img.youtube.com/vi/${video.vidId}/mqdefault.jpg`}
+                      />
+                      <HoverContainer>
+                        <StyledHoverText>VISA</StyledHoverText>
+                      </HoverContainer>
+                    </ThumbnailContainer>
+                    <StyledVideoTitle>{video.vidTitle}</StyledVideoTitle>
+                    <StyledVideoText>
+                      Du har gjort denna: {video.amountOfTimes} gånger{" "}
+                    </StyledVideoText>
+                  </VideoContainer>
                 </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </Wrapper>
+              );
+            })}
+          </SearchResultContainer>
+        </Wrapper>
+      </StyledContentContainer>
     </PatientLayout>
   );
 };
