2aae58d7c83b9f4602f43cac5760fb3664b0ca1e
viktorpolak98 <61971346+viktorpolak98@users.noreply.github.com>
2021-12-23 10:40:07 +0100

    flexbox update

--- client/src/components/admin/QRCreator.js
+++ client/src/components/admin/QRCreator.js
@@ -2,25 +2,44 @@ import React from "react";
 import QRCode from "react-qr-code";
 import styled from "styled-components";
 
-import { QrFlexbox } from "../common/Flexbox";
+import {
+  StyledCardFlexContainer,
+  StyledCardItem,
+  StyledCardHalfContainer,
+} from "../common/Flexbox";
 
 const StyledH1 = styled.h1`
-    margin: 0.2em;
-    font-family: "Roboto", sans-serif;
-    font-size: 5em;
-    text-align: center;
+  margin: 0.2em;
+  font-family: "Roboto", sans-serif;
+  font-size: 5em;
+  text-align: center;
+`;
+
+const StyledParagraph = styled.p`
+  font-family: "Roboto", sans-serif;
+  text-align: left;
+  font-size: 1em;
 `;
 
 const QRCreator = (props) => {
-    return (
-        <>
+  return (
+    <>
+      <StyledCardFlexContainer>
+        <StyledCardHalfContainer>
+          <div>
             <StyledH1>{props.text}</StyledH1>
-            <QrFlexbox>
-                <img src={props.image} alt={props.image} width="25%" />
-                <QRCode value={props.id} marginWidth={20} />
-            </QrFlexbox>
-        </>
-    );
+          </div>
+          <img src={props.image} alt={props.image} width="40%" />
+          <QRCode value={props.id} marginWidth={20} />
+        </StyledCardHalfContainer>
+        <StyledCardHalfContainer justify-content={"center"}>
+          <StyledCardItem>
+            <StyledParagraph></StyledParagraph>
+          </StyledCardItem>
+        </StyledCardHalfContainer>
+      </StyledCardFlexContainer>
+    </>
+  );
 };
 
 export default QRCreator;
--- client/src/components/common/Flexbox.js
+++ client/src/components/common/Flexbox.js
@@ -1,55 +1,96 @@
 import styled from "styled-components";
 
 const StyledFlexbox = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-content: flex-start;
-    align-items: baseline;
-    flex-wrap: wrap;
-    margin: 1em;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  align-items: baseline;
+  flex-wrap: wrap;
+  margin: 1em;
 `;
 
-const StyledQrFlexbox = styled(StyledFlexbox)`
-    margin-left: 20em;
-    margin-right: 20em;
-    align-items: center;
-    justify-content: space-evenly;
+const StyledCardFlexbox = styled(StyledFlexbox)`
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const CardFlexContainer = styled(StyledFlexbox)`
+  width: 210mm;
+  height: 258mm;
+`;
+
+const CardHalfContainer = styled(StyledCardFlexbox)`
+  height: 50%;
+`;
+
+const CardItem = styled.div`
+  margin: 2em;
 `;
 
 const FlexItem = styled.div`
-    margin-left: 0.5em;
-    margin-right: 0.5em;
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-    padding-top: 0.5em;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  padding-top: 0.5em;
 
-    :hover {
-        filter: brightness(130%);
-        background-color: ${(props) => props.theme.palette.hover};
-    }
+  :hover {
+    filter: brightness(130%);
+    background-color: ${(props) => props.theme.palette.hover};
+  }
 `;
+
+export const StyledCardHalfContainer = (props) => {
+  return (
+    <>
+      <CardHalfContainer {...props} />
+    </>
+  );
+};
+
+export const StyledCardItem = (props) => {
+  return (
+    <>
+      <CardItem {...props} />
+    </>
+  );
+};
+
+export const StyledCardFlexContainer = (props) => {
+  return (
+    <>
+      <CardFlexContainer {...props} />
+    </>
+  );
+};
+
 export const VideoItem = (props) => {
-    return (
-        <>
-            <FlexItem {...props} />
-        </>
-    );
+  return (
+    <>
+      <FlexItem {...props} />
+    </>
+  );
 };
 
 export const QrFlexbox = (props) => {
-    return (
-        <>
-            <StyledQrFlexbox {...props} />
-        </>
-    );
+  return (
+    <>
+      <StyledCardFlexbox {...props} />
+    </>
+  );
 };
 
 export const Flexbox = (props) => {
-    return (
-        <>
-            <StyledFlexbox {...props} />
-        </>
-    );
+  return (
+    <>
+      <StyledFlexbox {...props} />
+    </>
+  );
 };
 
-export default Flexbox + QrFlexbox + VideoItem;
+export default Flexbox +
+  QrFlexbox +
+  VideoItem +
+  StyledCardHalfContainer +
+  StyledCardItem +
+  StyledCardFlexContainer;
